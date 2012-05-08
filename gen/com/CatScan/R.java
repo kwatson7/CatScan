105767d4f2c6831c16a3022461ed27b22110f08a
Kyle Watson <kwatson7@gmail.com>
2012-05-08 08:07:24 -0700

    sync

--- gen/com/CatScan/R.java
+++ gen/com/CatScan/R.java
@@ -11,12 +11,12 @@ public final class R {
     public static final class attr {
     }
     public static final class color {
-        public static final int black25=0x7f040000;
-        public static final int gray=0x7f040001;
-        public static final int gray50=0x7f040003;
-        public static final int gray75=0x7f040002;
-        public static final int turquoise_bright=0x7f040004;
-        public static final int turquoise_dark=0x7f040005;
+        public static final int black25=0x7f060000;
+        public static final int gray=0x7f060001;
+        public static final int gray50=0x7f060003;
+        public static final int gray75=0x7f060002;
+        public static final int turquoise_bright=0x7f060004;
+        public static final int turquoise_dark=0x7f060005;
     }
     public static final class drawable {
         public static final int blue_background=0x7f020000;
@@ -77,21 +77,25 @@ public final class R {
     public static final class layout {
         public static final int caption_text_view=0x7f030000;
         public static final int edit_captions=0x7f030001;
-        public static final int picture_item=0x7f030002;
-        public static final int pictures_thread=0x7f030003;
-        public static final int title_layout=0x7f030004;
+        public static final int main=0x7f030002;
+        public static final int picture_item=0x7f030003;
+        public static final int pictures_thread=0x7f030004;
+        public static final int title_layout=0x7f030005;
     }
     public static final class string {
-        public static final int app_name=0x7f050001;
-        public static final int captionHint=0x7f050003;
-        public static final int hello=0x7f050000;
-        public static final int titleHint=0x7f050002;
+        public static final int app_name=0x7f040001;
+        public static final int captionHint=0x7f040003;
+        public static final int hello=0x7f040000;
+        public static final int titleHint=0x7f040002;
     }
     public static final class style {
-        public static final int LargeTitleTheme=0x7f060002;
-        public static final int MyTheme=0x7f060001;
-        public static final int Theme_OTPMain=0x7f060004;
-        public static final int TitlebarBackgroundStyle=0x7f060003;
-        public static final int WindowTitleBackground=0x7f060000;
+        public static final int CenteredDialog=0x7f050000;
+        public static final int CenteredDialogTitle=0x7f050001;
+        public static final int DialogWindowTitle=0x7f050002;
+        public static final int LargeTitleTheme=0x7f050005;
+        public static final int MyTheme=0x7f050004;
+        public static final int Theme_OTPMain=0x7f050007;
+        public static final int TitlebarBackgroundStyle=0x7f050006;
+        public static final int WindowTitleBackground=0x7f050003;
     }
 }
